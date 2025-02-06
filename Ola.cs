771f72790dd4e63113613c0377de41bb84155b56
Edgar Rodrigues <edgarodgui@gmail.com>
2025-02-06 10:50:12 -0300

    Adicionando arquivo

--- Ola.cs
+++ Ola.cs
@@ -1,10 +1,27 @@
 namespace Ola
 {
     class Cumprimento
-    {
-        public static void Mensagem()
-        {
-            Console.WriteLine("Olá!");
+    
+    public static void Somar
+      decimal valor1
+      decimal valor2
+      {
+       Console.Writeline($"A soma dos valores{valor1} e {valor2} e {valor1+valor2}")
+      }
+
+      {
+       Console.WriteLine($"A subtracao dos valores{valor1} e {valor2} e {valor1-valor2}")
+      }
+
+      {
+       Console.WriteLine($"A multiplicacao dos valores{valor1} e {valor2} e {valor1*valor2}")
+      }
+
+      {
+       Console.WriteLine($"A divisao dos valores{valor1} e {valor2} e {valor1/valor2}")
+      }
+
+
         }
     }
 }
